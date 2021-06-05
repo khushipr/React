66976bee76ae2ef2e47cb012cbad56bcc4140c78
khushipr <khushipr01@gmail.com>
2021-06-05 20:25:32 +0530

    added tours

---
+++ tours/src/db/data.js
@@ -0,0 +1,34 @@
+import paris from '../images/paris.jpeg'
+import ire from '../images/ireland.jpeg'
+import switz from '../images/switz.jpeg'
+import gre from '../images/greece.jpeg'
+export default [
+        {
+            "name" : "Best of Paris in 7 days tour",
+            "info" : "Paris – the city of culture, art, and romance. This Paris itinerary for 7 days will guide you on how to see, do and taste and snap the best pictures in the beautiful capital of France.Whether sunshine is sparkling on the café terraces of Boulevard Saint-Germain, or melancholy mists of the Seine River are shrouding Notre-Dame Cathedral, Paris has a way of romancing visitors. The love affair may begin with a first glimpse of the Eiffel Tower, then continue with strolls along the wide tree-lined avenues and in lavish formal gardens.",
+            "image" : paris,
+            "price" : "2,995",
+            "id" : 1
+        },
+        {
+            "name" : "Best of Ireland in 14 days tour",
+            "info" : "Welcoming and wonderfully rich in culture, Ireland, the Emerald Isle, is sure to put a sparkle in your eye. You'll love its friendly people; laid-back attitude; often tragic yet fascinating history; and its rugged, romantic landscapes. This is the land of saints and scholars.Places to visit and attractions for tourists are abundant and infinitely varied. The state museums are all free, heritage sites date to prehistory, and there are endless outdoor pursuits to enjoy countrywide, such as horse riding, golf, sailing, and numerous remote, wild islands to explore. And, of course, there's the famous Irish craic (good time) to enjoy, wherever you decide to go.",
+            "image" : ire,
+            "price" : "3,895",
+            "id" : 2
+        },
+        {
+            "name" : "Best of Switzerland in 7 days tour",
+            "info" : "For ravishing alpine scenery and quaint villages oozing storybook charm, it's hard to beat Switzerland. The soaring snow-capped peaks of the Alps, glittering blue lakes, emerald valleys, glaciers, and picturesque lakeside hamlets imbue this landlocked nation with a fairytale beauty. Snuggled amid the mountain valleys and lakes lie world-class international resorts and a long list of things to do, including hiking, biking, climbing, paragliding, skiing, and tobogganing. Travelers come here for jaw-dropping scenery but are charmed by the many cultural attractions. Steeped in history, cities such as Zurich, Geneva, and Lausanne offer excellent museums and galleries, historic buildings, and renowned music festivals.",
+            "image" : switz,
+            "price" : "4,695",
+            "id" : 3
+        },
+        {
+            "name" : "Best of Greece in 7 days tour",
+            "info" : "Home to some of the world's most important historical sites, along with some 6,000 islands, Greece is known for its natural beauty and fascinating culture. Ancient archaeological sites, cliffs tumbling into sparkling blue water, sand and pebble beaches, and a balmy Mediterranean climate make Greece one of Europe's prime tourist destinations. Besides the capital, Athens, top things to see on the mainland include Ancient Delphi and the monasteries of Meteora. But most people come here to catch a ferry to the islands; Santorini, Mykonos, Zakynthos, Corfu, and Crete are the most popular. Plan your trip with our list of the top attractions in Greece.",
+            "image" : gre,
+            "price" : "4,995",
+            "id" : 4
+        }
+    ]
